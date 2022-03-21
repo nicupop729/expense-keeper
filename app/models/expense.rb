9d4cc20047b4677528caeb41bbe27cf50ad60cae
Nicolae Pop <nicupop729@gmail.com>
2022-03-21 20:45:18 -0300

    Build tests for Expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,6 +3,6 @@ class Expense < ApplicationRecord
   has_many :category_expenses
   has_many :categories, through: :category_expenses
 
-  validates :name, presence: true
-  validates :amount, numericality: { greater_that_or_equal_to: 0, less_than: BigDecimal(10**6)}
+  validates :name, presence: true, length: { in: 3..25 }
+  validates :amount, presence: true, numericality: { greater_than: 0, less_than: BigDecimal(10**6)}
 end
