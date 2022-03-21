b38f0dab95f3eee6fd1897c12a898c8cf6bcb93c
Nicolae Pop <nicupop729@gmail.com>
2022-03-21 18:50:59 -0300

    Add validations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,7 @@ class Expense < ApplicationRecord
   belongs_to :user
   has_many :category_expenses
   has_many :categories, through :category_expenses
+
+  validates :name, presence: true
+  validates :amount, numericality: { greater_that_or_equal_to: 0, less_than: BigDecimal(10**6)}
 end
