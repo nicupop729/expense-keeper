417b0e12b7c53227c70064c3fa8744e1bf5f988f
Nicolae Pop <nicupop729@gmail.com>
2022-03-21 23:48:11 -0300

    Build the first page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/index', to: 'pages#index'
   devise_for :users
   resources :categories
   resources :expenses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "categories#index"
+  root "pages#index"
 end
