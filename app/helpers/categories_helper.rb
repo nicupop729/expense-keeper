08393ec76593bc1bdebe46815ffa49eb29197093
Nicolae Pop <nicupop729@gmail.com>
2022-03-24 19:00:26 -0300

    Apply linter check

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,7 +1,7 @@
 module CategoriesHelper
-    def category_total(category)
+  def category_total(category)
     total_category = category.expenses.sum(:amount)
-      
+
     "$#{total_category}"
-    end
+  end
 end
